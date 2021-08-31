06bf6fe2df392173b6976a72e54f7e28ea027880
ImGukdo <85783104+ImGukdo@users.noreply.github.com>
2021-08-31 11:11:58 +0900

    5_그리기함수

---
+++ cpp/5_그리기함수.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+void drawLines() {
+	Mat img(400, 400, CV_8UC3, Scalar(255, 255, 255));
+
+	// 수평 방향 직선 그리기
+	line(img, Point(50, 50), Point(200, 50), Scalar(0, 0, 255));
+	line(img, Point(50, 100), Point(200, 100), Scalar(255, 0, 255), 3);
+	line(img, Point(50, 150), Point(200, 150), Scalar(255, 0, 0), 10);
+	
+	// 대각선 방향 직선 그리기
+	line(img, Point(250, 50), Point(350, 100), Scalar(0, 0, 255), 1, LINE_4);
+	line(img, Point(250, 70), Point(350, 120), Scalar(255, 0, 255), 1, LINE_8);
+	line(img, Point(250, 90), Point(350, 140), Scalar(255, 0, 0), 1, LINE_AA);
+
+	// 수평 방향 화살표 직선 그리기
+	arrowedLine(img, Point(50, 200), Point(150, 200), Scalar(0, 0, 255), 1);
+	arrowedLine(img, Point(50, 250), Point(350, 250), Scalar(255, 0, 255), 1);
+	arrowedLine(img, Point(50, 300), Point(350, 300), Scalar(255, 0, 0), 1, LINE_8, 0, 0.05);
+
+	// 다양한 모양의 마커 그리기
+	drawMarker(img, Point(50, 350), Scalar(0, 0, 255), MARKER_CROSS);
+	drawMarker(img, Point(100, 350), Scalar(0, 0, 255), MARKER_TILTED_CROSS);
+	drawMarker(img, Point(150, 350), Scalar(0, 0, 255), MARKER_STAR);
+	drawMarker(img, Point(200, 350), Scalar(0, 0, 255), MARKER_DIAMOND);
+	drawMarker(img, Point(250, 350), Scalar(0, 0, 255), MARKER_SQUARE);
+	drawMarker(img, Point(300, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_UP);
+	drawMarker(img, Point(350, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_DOWN);
+
+	imshow("img", img);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+void drawPolys() {
+	Mat img(400, 400, CV_8UC3, Scalar(255, 255, 255));
+	// 두께가 -1이면 내부를 채운다
+	// 사각형 그리기
+	rectangle(img, Rect(50, 50, 100, 50), Scalar(0, 0, 255), 2);
+	rectangle(img, Rect(50, 150, 100, 50), Scalar(0, 0, 128), -1);
+
+	// 원 그리기
+	circle(img, Point(300, 120), 30, Scalar(255, 255, 0), -1, LINE_AA);
+	circle(img, Point(300, 120), 60, Scalar(255, 0, 0), 3, LINE_AA);
+
+	// 타원 그리기
+	ellipse(img, Point(120, 300), Size(60, 30), 20, 0, 270, Scalar(255, 255, 0), -1, LINE_AA);
+	ellipse(img, Point(120, 300), Size(100, 50), 20, 0, 360, Scalar(0, 255, 0), 2, LINE_AA);
+
+	// 다각형 그리기
+	vector<Point> pts = {Point(250, 250), Point(300, 250), Point(300, 300), 
+							Point(350, 300), Point(350, 350), Point(250, 350)};
+	polylines(img, pts, true, Scalar(255, 0, 255), 2);
+
+	imshow("img", img);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+void drawText1()
+{
+	Mat img(500, 800, CV_8UC3, Scalar(255, 255, 255));
+	
+	// 다양한 폰트로 문자열 출력하기, 좌측하단점을 기준으로 출력
+	putText(img, "FONT_HERSHEY_SIMPLEX", Point(20, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+	putText(img, "FONT_HERSHEY_PLAIN", Point(20, 100), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
+	putText(img, "FONT_HERSHEY_DUPLEX", Point(20, 150), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255));
+	putText(img, "FONT_HERSHEY_COMPLEX", Point(20, 200), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 0, 0));
+	putText(img, "FONT_HERSHEY_TRIPLEX", Point(20, 250), FONT_HERSHEY_TRIPLEX, 1, Scalar(255, 0, 0));
+	putText(img, "FONT_HERSHEY_COMPLEX_SMALL", Point(20, 300), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 0, 0));
+	putText(img, "FONT_HERSHEY_SCRIPT_SIMPLEX", Point(20, 350), FONT_HERSHEY_SCRIPT_SIMPLEX, 1, Scalar(255, 0, 255));
+	putText(img, "FONT_HERSHEY_SCRIPT_COMPLEX", Point(20, 400), FONT_HERSHEY_SCRIPT_COMPLEX, 1, Scalar(255, 0, 255));
+	putText(img, "FONT_HERSHEY_COMPLEX | FONT_ITALIC", Point(20, 450), FONT_HERSHEY_COMPLEX | FONT_ITALIC, 1, Scalar(255, 0, 0));
+
+	imshow("img", img);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+void drawText2() {
+	Mat img(200, 640, CV_8UC3, Scalar(255, 255, 255));
+
+	const String text = "Hello, OpenCV";
+	int fontFace = FONT_HERSHEY_TRIPLEX;
+	double fontScale = 2.0;
+	int thickness = 1;
+	
+	Size sizeText = getTextSize(text, fontFace, fontScale, thickness, 0);  // 출력할 문자열이 차지할 사각형 크기 
+	Size sizeImg = img.size();
+
+	Point org((sizeImg.width - sizeText.width) / 2, (sizeImg.height + sizeText.height) / 2);  // 문자열의 좌측 하단점 구하기 
+
+	putText(img, text, org, fontFace, fontScale, Scalar(255, 0, 0), thickness);  // 문자열 출력 
+	rectangle(img, org, org + Point(sizeText.width, -sizeText.height), Scalar(255, 0, 0), 1);  // 사각형 출력 
+
+	imshow("img", img);
+	waitKey();
+
+	destroyAllWindows();
+}
+
+int main(void) {
+	// drawLines();
+	// drawPolys();
+	// drawText1();
+	drawText2();
+
+	return 0;
+}
